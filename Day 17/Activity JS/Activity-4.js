e74a0dcc9ef6431b52bfb164f4dc064b0bfc4c0a
Subham <subhamoffi@gmail.com>
2024-07-29 20:21:00 +0530

    Day 17 done except the optional, I will do it tomorrow

--- Day 17/Activity JS/Activity-4.js
+++ Day 17/Activity JS/Activity-4.js
@@ -12,16 +12,70 @@ class TreeNode {
 class BinaryTree {
     constructor() {
         this.stem = null;
-
     }
 
     insertValue(value) {
-        this.stem = new TreeNode(value, this.stem, this.stem)
+        let node = new TreeNode(value)
+        this.mainAddFun(node, this.stem)
+
     }
+
+    mainAddFun(node, branch = null) {
+        let root = branch;
+        if(root == null) {
+            this.stem = node;
+        }
+        else{
+            this.loopFunction(root, node)
+        }
+
+    }
+
+    loopFunction(root, node) {
+        if(root.leftBranch == null && root.rightBranch == null) {
+            root.leftBranch = node;
+
+        }
+        else if(root.leftBranch != null && root.rightBranch == null) {
+            root.rightBranch = node;
+
+        }
+        else if (root.leftBranch != null && root.rightBranch != null) {
+            if (root.value > node.value) {
+                this.mainAddFun(node, root.rightBranch)
+            }
+            else{
+                this.mainAddFun(node, root.leftBranch);
+            }
+        }
+    }
+
+    inOrderDisplay() {
+        let node = this.stem;
+        this.display(node);
+    }
+
+    display(node) {
+        if(node) {
+            this.display(node.leftBranch);
+            console.log(node.value)
+            this.display(node.rightBranch)
+        }
+    }
+
 }
 
 const tree = new BinaryTree();
 
 tree.insertValue(12);
 tree.insertValue(34);
+tree.insertValue(10);
+tree.insertValue(11);
+tree.insertValue(13);
+tree.insertValue(33);
+tree.insertValue(35);
+tree.insertValue(9);
+
 console.log(tree);
+
+tree.inOrderDisplay();
