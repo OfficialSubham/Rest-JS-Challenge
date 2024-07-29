e7308a127b53844ef9b4ad8b4b0f60e5a6b96584
Subham <subhamoffi@gmail.com>
2024-07-29 17:43:22 +0530

    Activity 4 and 5 is remaining

--- Day 17/Activity JS/Activity-4.js
+++ Day 17/Activity JS/Activity-4.js
@@ -1,1 +1,27 @@
-//Task 7
+//Task 7 && Task 8 Combined
+
+class TreeNode {
+    constructor(value, left = null, right = null) {
+        this.leftBranch = left;
+        this.value = value;
+        this.rightBranch = right;
+    }
+
+}
+
+class BinaryTree {
+    constructor() {
+        this.stem = null;
+
+    }
+
+    insertValue(value) {
+        this.stem = new TreeNode(value, this.stem, this.stem)
+    }
+}
+
+const tree = new BinaryTree();
+
+tree.insertValue(12);
+tree.insertValue(34);
+console.log(tree);
