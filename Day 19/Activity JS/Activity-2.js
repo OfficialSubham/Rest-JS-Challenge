1c4c99216f9466e91778c31931f9d9bb0371d25f
Subham <subhamoffi@gmail.com>
2024-07-31 20:27:15 +0530

    Character Classes and Quantifiers Done

---
+++ Day 19/Activity JS/Activity-2.js
@@ -0,0 +1,13 @@
+//Task 3
+
+let regex = /[A-Z]/g;
+const string1 = "Hello Guys How Are You"
+let matches = string1.match(regex);
+console.log(matches);
+
+//Task 4
+
+let regex2 = /\d+/g;
+const string2 = "this is 123 and this is 321 and this is 213"
+let matches2 = string2.match(regex2);
+console.log(matches2);
