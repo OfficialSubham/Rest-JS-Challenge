e8d66ee691da826a5f3179b4f125e626982df2fd
Subham <subhamoffi@gmail.com>
2024-08-01 07:53:41 +0530

    Using SessionStorage Done

--- Day 20/Activity JS/Activity-4.js
+++ Day 20/Activity JS/Activity-4.js
@@ -1,1 +1,23 @@
+//Task 7
+
+let form = {
+
+}
+
+document.querySelector("button")
+    .addEventListener('click', () => {
+        form.name = document.querySelector(".name-input").value
+        form.email = document.querySelector(".email-input").value
+        sessionStorage.setItem("form", JSON.stringify(form));
+
+        let data = JSON.parse(sessionStorage.getItem("form"));
+        console.log(data);
+    })
  
+let data = JSON.parse(sessionStorage.getItem("form"));
+console.log(data);
+
+//Task 8
+
+let newData = sessionStorage.removeItem("form");
+console.log(newData);
