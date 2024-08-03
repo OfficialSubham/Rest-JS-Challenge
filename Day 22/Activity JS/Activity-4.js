8055125e2950a90645abd0ff3c41206ed1211f04
Subham <subhamoffi@gmail.com>
2024-08-03 17:26:13 +0530

    I will do the DSA problems later as I am into Fullstack for now

--- Day 22/Activity JS/Activity-4.js
+++ Day 22/Activity JS/Activity-4.js
@@ -5,30 +5,25 @@
 let array = [-3, 3, 4, -3, 1, 2] //defining the array
 
 function threeSum(array) {
-    //according to bubble sort algorithm
-    let tempArray = [];
-    let sortedArray = array;
-
-    for(let i = 0; i < sortedArray.length; i++) {
-        for(let index = 0; index < sortedArray.length; index++) {
-            if(sortedArray[index] > sortedArray[index + 1]) {
-                tempArray.push(sortedArray[index])
-                sortedArray[index] = sortedArray[index + 1];
-                sortedArray[index + 1] = tempArray[0];
-                tempArray = [];
+   
+    let HashMap = {};
+    let tripletsArray = [];
+    for (let i = 0; i < array.length; i++) {
+        let target = array[i];
+        for (let index = (i + 1); index < array.length; index++) {
+            if ((target + array[index]) in HashMap) {
+                tripletsArray.push(target);
+                tripletsArray.push(HashMap[target + array[index]])
+                tripletsArray.push(array[index]);
+                HashMap[array[index]] = index;
+            }
+            else {
+                HashMap[array[index]] = index;
             }
         }
     }
-    
-    let tripletArray = [];//here we get the threesum
-
-    for(let i = 0; i < sortedArray.length; i++) {
-        let firstNumber = sortedArray[i];
-        let secondNumber;
-        let thirdNumber;
-        for(let index = 0; index < sortedArray.length; index++) {
-        }
-    }
+    console.log(HashMap);
+    console.log(tripletsArray);
 
 }
 
