1311788fefb6e65b2c5a0c10778ae3f95f660cfb
Subham <subhamoffi@gmail.com>
2024-08-03 16:15:10 +0530

    Done Two Sum Using HashMap

--- Day 21/Activity JS/Activity-1.js
+++ Day 21/Activity JS/Activity-1.js
@@ -29,3 +29,31 @@ function twoSum(array, target) {
 let array = [1, 2, 3, 4, 5]
 
 twoSum(array, 5);
+
+//Using #MAP
+
+function TwoSumHashMap(array, target) {
+    let map = {}; // variable : indices
+
+    let indicesArray = []; //this array gives us the indces
+    //this loop is just going through the arry one time
+    for(let i = 0; i < array.length; i++) {
+        if((target - array[i]) in map) {
+            //then it is checking that the 
+            //value after substracting is in the array so that 
+            //then it will push both of the indices to the array
+            indicesArray.push(i);
+            indicesArray.push(map[target - array[i]]);
+
+        }
+        else {
+            //if the array does not contain the integer then it will add
+            //to the object
+            map[array[i]] = i;
+        }
+    }
+
+    console.log(indicesArray);
+}
+
+TwoSumHashMap(array, 5)
